09e55e64ce54003f8833d581702177726126cee3
Daniel070999 <danielixo99@gmail.com>
2023-10-12 21:20:04 -0500

    taller 1

---
+++ taller1/scripts.js
@@ -0,0 +1,31 @@
+const API = 'https://swapi.dev/api/people/';
+
+function personaje(texto) {
+  let div = document.createElement('div');
+  let h1_texto = document.createTextNode(texto.name);
+  let h1 = document.createElement('h1');
+  h1.appendChild(h1_texto);
+  div.appendChild(h1);
+
+  let contenedor = document.getElementById('contenedor');
+  contenedor.appendChild(div);
+}
+
+async function obtener_personaje(id) {
+  try {
+    let response = await fetch(`${API}${id}`);
+    let data = await response.json();
+    personaje(data);
+  } catch (error) {
+    console.error(`[error]: ${error}`);
+  }
+}
+
+async function obtener_personajes() {
+  console.log('asd');
+  for (let i = 1; i <= 80; i++) {
+    await obtener_personaje(i);
+  }
+}
+
+obtener_personajes();
