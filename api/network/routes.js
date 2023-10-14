1b976ee449bc66ae759c17aa4cfee2552c5ea330
Daniel070999 <danielixo99@gmail.com>
2023-10-14 10:26:28 -0500

    taller 2

--- api/network/routes.js
+++ api/network/routes.js
@@ -1,9 +1,13 @@
 const usuario = require('../components/usuario/interface')
 const pais = require('../components/pais/interface')
+const empresa = require('../components/empresa/interface')
+const representantelegal = require('../components/representantelegal/interface')
 
-const routes = function( server ) {
+const routes = function (server) {
     server.use('/usuario', usuario)
     server.use('/pais', pais)
+    server.use('/empresa', empresa)
+    server.use('/representantelegal', representantelegal)
 }
 
 module.exports = routes
