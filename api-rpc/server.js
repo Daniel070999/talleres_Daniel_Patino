4b78298670f500263e5448a28c1edd79eb80cfa0
Daniel070999 <danielixo99@gmail.com>
2023-11-10 20:22:45 -0500

    practica gRPC

--- api-rpc/server.js
+++ api-rpc/server.js
@@ -1,40 +1,39 @@
 import grpc from '@grpc/grpc-js'
-import protoloader from '@grpc/proto-loader'
+import protoLoader from '@grpc/proto-loader'
 
 const PROTO_FILE = './proto/user.proto'
 
 async function main() {
-    const packageDefinition = protoloader.loadSync(PROTO_FILE, {
+    const packageDefinition = protoLoader.loadSync(PROTO_FILE, {
         keepCase: true,
         longs: String,
         enums: String,
-        arrays: true
+        arrays: true,
     })
 
     const userProto = grpc.loadPackageDefinition(packageDefinition)
     const server = new grpc.Server()
 
-    let users = [
-        { name: "Daniel Patiño", email: "dpatinov@ups.edu.ec", age: 24 }
-    ]
+    let users = []
 
     server.addService(userProto.UserService.service, {
         getUsers: (_, callback) => {
-            callback(null, { users })
+            console.log(users);
+            callback(null, { Users: users })
         },
         addUser: (call, callback) => {
             const user = call.request
             users.push(user)
             callback(null, user)
         }
-    })
+    });
 
     await server.bindAsync(
         '127.0.0.1:3043',
         grpc.ServerCredentials.createInsecure(),
         () => {
             server.start()
-            console.log('Server RPC on port 3043');
+            console.log('Server RPC on port 3043')
         }
     )
 }
